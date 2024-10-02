1e2da452106128f2e411b535a1dc5122bd3947e5
purva1903 <purvachalke19@gmail.com>
2024-10-02 19:01:39 +0530

    new updation 1

--- Day1/q1_1_evenodd.php
+++ Day1/q1_1_evenodd.php
@@ -4,7 +4,7 @@ $no= array(11,23,72,44);
 for($i=0;$i<=3;$i++)
 {
 if($no[$i]%2 ==1)
-  echo "The number " .$no[$i].  " is an odd number<br>";
+    echo "The number " .$no[$i].  " is an odd number<br>";
 
 else
 	echo"The number " .$no[$i]. " is even number<br>";
